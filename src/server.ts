7063971e065f39afc75cc2eb5f17df1e649ef192
Geraldo <geraldovilela19@gmail.com>
2021-11-21 08:41:33 -0400

    dockerfile and docker-compose initial;

---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  return res.json({ message: "API is Running" })
+})
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).json({
+      error: err.message,
+      name: err.name,
+      stack: err.stack
+    })
+  }
+
+  return res.status(500).json({
+    message: "Internal server error"
+  })
+});
+
+app.listen(3333, () => {
+  console.log("🚀 😜 🚀 Server is running on docker")
+})
